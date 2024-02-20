885ed419e30f8b633dbafae21589c696a2300c7c
Joanne Gangapal <joannegangapal@ensc.fr>
2024-02-20 17:44:23 +0100

    Aperçu

--- fluidservice/src/Reservation.js
+++ fluidservice/src/Reservation.js
@@ -77,7 +77,7 @@ function Calendrier (){
 return (<div>
     <div className='Table-Header'>
             <h2>{moisActuel}</h2>
-            <div><input type='checkbox' id='Arrive'/>Arrivé <input type='checkbox' id='Annule'/>Annulé <input type='checkbox' id='Prevu'/>Prévue</div>
+            <div><input type='checkbox' id='Arrive'/><h6 className='Arrive'>Arrivé </h6><input type='checkbox' id='Annule'/><h6 className='Annule'>Annulé</h6> <input type='checkbox' id='Prevu'/>Prévue</div>
 <div>Aujourd'hui precedent suivant</div>
         </div>
         <div>
@@ -95,40 +95,54 @@ return (<div>
 </thead>
 <tbody>
     <tr>
+        <td>29</td>
+        <td>30</td>
+        <td>31</td>
         <td>1</td>
         <td>2</td>
         <td>3</td>
         <td>4</td>
-        <td>5</td>
-        <td>6</td>
-        <td>7</td>
     </tr>
     <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
         <td>5</td>
         <td>6</td>
         <td>7</td>
+        <td>8</td>
+        <td>9</td>
+        <td>10</td>
+          <td>11</td>
     </tr>
     <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
-        <td>7</td>
+        
+        <td>12</td>
+        <td>13</td>
+        <td>14</td>
+        <td>15</td>
+        <td>16</td>
+        <td>17
+            <hr className='Arrive'></hr>
+        </td>
+        <td>18</td>
     </tr>
     <tr>
+        
+        <td>19</td>
+        <td className='Date-jour'>20</td>
+        <td>21</td>
+        <td>22</td>
+        <td ><hr className='Annule'></hr>23</td>
+        <td>24</td>
+        <td>25</td>
+    </tr>
+    <tr>
+        
+        <td>26</td>
+        <td>27</td>
+        <td>28</td>
+        <td>29</td>
         <td>1</td>
         <td>2</td>
         <td>3</td>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
-        <td>7</td>
     </tr>
 </tbody>
 
